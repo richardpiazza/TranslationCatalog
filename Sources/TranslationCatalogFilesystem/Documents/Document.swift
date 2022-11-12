de7f01736a4a27848f550152384d7fe8f00a0b60
Richard Piazza <richard@richardpiazza.com>
2022-11-12 13:16:14 -0600

    Simplified File URLs

--- Sources/TranslationCatalogFilesystem/Documents/Document.swift
+++ Sources/TranslationCatalogFilesystem/Documents/Document.swift
@@ -9,35 +9,12 @@ extension Document {
     
     func write(to directory: URL, using encoder: JSONEncoder) throws {
         let data = try encoder.encode(self)
-        
-        let url: URL
-        
-        #if swift(>=5.7.1)
-        if #available(macOS 13.0, iOS 16.0, tvOS 16.0, watchOS 9.0, *) {
-            url = directory.appending(path: filename, directoryHint: .notDirectory)
-        } else {
-            url = directory.appendingPathComponent(filename)
-        }
-        #else
-        url = directory.appendingPathComponent(filename)
-        #endif
-        
+        let url = URL(fileURLWithPath: directory.appendingPathComponent(filename).path)
         try data.write(to: url)
     }
     
     func remove(from directory: URL) throws {
-        let url: URL
-        
-        #if swift(>=5.7.1)
-        if #available(macOS 13.0, iOS 16.0, tvOS 16.0, watchOS 9.0, *) {
-            url = directory.appending(path: filename, directoryHint: .notDirectory)
-        } else {
-            url = directory.appendingPathComponent(filename)
-        }
-        #else
-        url = directory.appendingPathComponent(filename)
-        #endif
-        
+        let url = URL(fileURLWithPath: directory.appendingPathComponent(filename).path)
         try FileManager.default.removeItem(at: url)
     }
 }
