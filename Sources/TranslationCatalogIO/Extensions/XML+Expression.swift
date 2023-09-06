8a7c833d98ffc8bda88b9d967bdcbab29502ef53
Richard Piazza <richard@richardpiazza.com>
2023-09-06 10:43:15 -0500

    Simple Android XML Escapes (#10)
    * Simple Android XML Escapes
    
    * Disabled extra test case

--- Sources/TranslationCatalogIO/Extensions/XML+Expression.swift
+++ Sources/TranslationCatalogIO/Extensions/XML+Expression.swift
@@ -9,10 +9,31 @@ extension XML {
                 .forEach(expressions) {
                     .element(named: "string", nodes: [
                         .attribute(named: "name", value: $0.key),
-                        .text($0.translations.first?.value ?? "")
+                        .text(($0.translations.first?.value ?? "").simpleAndroidXMLEscaped())
                     ])
                 }
             ])
         )
+    }
+}
+
+internal extension String {
+    func simpleAndroidXMLEscaped() -> String {
+        let replacements: [(Character, String)] = [
+            ("&", "&amp;"),
+//            ("\"", "&quot;"),
+            ("'", "\\'"),
+            ("<", "&lt;"),
+            (">", "&gt;"),
+            (" ", "&#160;") // NBSP
+        ]
+        
+        var updated = self
+        
+        for (character, replacement) in replacements {
+            updated = updated.replacingOccurrences(of: String(describing: character), with: replacement, range: nil)
+        }
+        
+        return updated
     }
 }
---
+++ Tests/TranslationCatalogTests/XMLEscapingTests.swift
@@ -0,0 +1,14 @@
+import XCTest
+@testable import TranslationCatalogIO
+
+final class XMLEscapingTests: XCTestCase {
+    
+    func testSimpleEscaping() {
+        XCTAssertEqual("I am not Spartacus & lying".simpleAndroidXMLEscaped(), "I am not Spartacus &amp; lying")
+//        XCTAssertEqual(#""I am Spartacus""#.simpleAndroidXMLEscaped(), "&quot;I am Spartacus&quot;")
+        XCTAssertEqual("I'm Spartacus".simpleAndroidXMLEscaped(), #"I\'m Spartacus"#)
+        XCTAssertEqual("I < Spartacus".simpleAndroidXMLEscaped(), "I &lt; Spartacus")
+        XCTAssertEqual("Spartacus > I".simpleAndroidXMLEscaped(), "Spartacus &gt; I")
+        XCTAssertEqual("Spartacus is me".simpleAndroidXMLEscaped(), "Spartacus&#160;is&#160;me")
+    }
+}
