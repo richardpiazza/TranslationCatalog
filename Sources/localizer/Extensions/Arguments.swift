4ad9a7dcd73c25e451d36fbb556c225294f8a2b1
Richard Piazza <richard@richardpiazza.com>
2025-02-19 09:49:13 -0600

    Check for retroactive feature

--- Sources/localizer/Extensions/Arguments.swift
+++ Sources/localizer/Extensions/Arguments.swift
@@ -2,6 +2,7 @@ import Foundation
 import LocaleSupport
 import ArgumentParser
 
+#if hasFeature(RetroactiveAttribute)
 extension LanguageCode: @retroactive ExpressibleByArgument {}
 extension RegionCode: @retroactive ExpressibleByArgument {}
 extension ScriptCode: @retroactive ExpressibleByArgument {}
@@ -10,3 +11,13 @@ extension UUID: @retroactive ExpressibleByArgument {
         self.init(uuidString: argument)
     }
 }
+#else
+extension LanguageCode: ExpressibleByArgument {}
+extension RegionCode: ExpressibleByArgument {}
+extension ScriptCode: ExpressibleByArgument {}
+extension UUID: ExpressibleByArgument {
+    public init?(argument: String) {
+        self.init(uuidString: argument)
+    }
+}
+#endif
