13aff094727a8769f21f192c0e9f8955d465dcf7
Richard Piazza <richard@richardpiazza.com>
2025-07-20 08:11:10 -0500

    Additional testing resources

--- Package.swift
+++ Package.swift
@@ -116,6 +116,9 @@ let package = Package(
                 "TranslationCatalogIO",
                 "TranslationCatalogFilesystem",
                 "TranslationCatalogSQLite",
+            ],
+            resources: [
+                .process("Resources"),
             ]
         ),
     ],
